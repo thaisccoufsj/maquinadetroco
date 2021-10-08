a43e5759812ccede3a825fbbf08331ed76e0fd88
thaisccoufsj@gmail.com <Comp@kar2512>
2021-10-07 22:18:41 -0300

    Inicio

---
+++ app/src/main/java/com/example/maquinadetroco/Caixa/CaixaActivity.java
@@ -0,0 +1,16 @@
+package com.example.maquinadetroco.Caixa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.maquinadetroco.R;
+
+public class CaixaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caixa);
+    }
+}
